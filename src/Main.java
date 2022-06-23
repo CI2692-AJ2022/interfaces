4687e4c9a59f16a9b258037f2197bae5e67f3a5e
corderomiguel <15-10326@usb.ve>
2022-06-23 19:27:34 -0400

    Class Employee and Test to implement a company structure of employee and subordinate

--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,25 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Testing queues
-        System.out.println("\n ---QUEUES USING ARRAY---");
-        MyQueue queue = new MyQueue();
-        queue.add(2);
-        queue.add(3);
-        queue.add(7);
+        // Create all employees in company
+        Employee empleado1 = new Employee("Fulano",46,2000);
+        Employee empleado2 = new Employee("Mengano",50,1000);
+        Employee empleado3 = new Employee("Sotano",51,1000);
+        Employee empleado4 = new Employee("Pengano",52,1000);
 
-        System.out.println("\n Last Element in Queue"+ " " + queue.peek());
-        System.out.println("Delete last element");
-        queue.poll();
-        System.out.println("\n Last Element in Queue"+ " " + queue.peek());
-        System.out.println("Delete last element");
-        queue.poll();
-        System.out.println("\n Last Element in Queue"+ " " + queue.peek());
-        System.out.println("Delete last element");
-        queue.poll();
-        // Queue must be empty
-        System.out.println("\n Last Element in Queue"+ " " + queue.peek());
-        System.out.println("Delete last element");
-        queue.poll();
+        // Assign only DIRECT subordinates for an employee
+        empleado1.setSubordinates(empleado2);
+        empleado2.setSubordinates(empleado3);
+        empleado3.setSubordinates(empleado4);
 
-
-        System.out.println("\n ---QUEUE USING ARRAY LIST---");
-        MyArrayListQueue queueList = new MyArrayListQueue();
-        queueList.add(2);
-        queueList.add(3);
-        queueList.add(7);
-        System.out.println("\n Last Element in Queue" + " " + queueList.peek());
-        System.out.println("Delete last element");
-        queueList.poll();
-        System.out.println("\n Last Element in Queue" + " " + queueList.peek());
-        System.out.println("Delete last element");
-        queueList.poll();
-        System.out.println("\n Last Element in Queue" + " " + queueList.peek());
-        System.out.println("Delete last element");
-        queueList.poll();
-        // Queue must be empty
-        System.out.println("\n Last Element in Queue" + " " + queueList.peek());
-        System.out.println("Delete last element");
-        queueList.poll();
-
-        // Testing Stack
-
-
-        System.out.println("\n ---STACK USING ARRAYS---");
-        MyStack stack = new MyStack();
-        stack.push(2);
-        stack.push(3);
-        stack.push(7);
-
-        System.out.println("\n Last Element in Stack" + " " + stack.peek());
-        System.out.println("Delete last element");
-        stack.pop();
-        System.out.println("\n Last Element in Stack" + " " + stack.peek());
-        System.out.println("Delete last element");
-        stack.pop();
-        System.out.println("\n Last Element in Stack" + " " + stack.peek());
-        System.out.println("Delete last element");
-        stack.pop();
-        // Stack must be empty
-        System.out.println("\n Last Element in Stack" + " " + stack.peek());
-        System.out.println("Delete last element");
-        stack.pop();
-
-
-        System.out.println("\n ---STACK USING ARRAY LIST---");
-        MyArrayListStack stackList = new MyArrayListStack();
-        stackList.push(2);
-        stackList.push(3);
-        stackList.push(7);
-
-        System.out.println("\n Last Element in Stack" +  " " + stackList.peek());
-        System.out.println("Delete last element");
-        stackList.pop();
-        System.out.println("\n Last Element in Stack" +  " " + stackList.peek());
-        System.out.println("Delete last element");
-        stackList.pop();
-        System.out.println("\n Last Element in Stack" +  " " + stackList.peek());
-        System.out.println("Delete last element");
-        stackList.pop();
-        // Stack must be empty
-        System.out.println("\n Last Element in Stack" +  " " + stackList.peek());
-        System.out.println("Delete last element");
-        stackList.pop();
-
+        // Test of two implementation
+        System.out.println("\n Calculando los subornidados aplicando recursiòn");
+        System.out.println(empleado1.employeesOldLowerRecursive(empleado1));
+        System.out.println("\n Calculando los subordinados aplicando iteración y queues ");
+        System.out.println(empleado1.employeesOldLower(empleado1));
 
     }
 }
