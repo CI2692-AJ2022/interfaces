9e2b03f244fe255a73a0b06b95427b73d16ad00c
corderomiguel <15-10326@usb.ve>
2022-06-24 12:59:46 -0400

    Add random generator for employee data. And corrected queue application.
    Signed-off-by: corderomiguel <15-10326@usb.ve>

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *  Class Employee to implement a company structure of employee and subordinate
@@ -12,13 +13,39 @@ public class Employee {
     private ArrayList<Employee> subordinates;
 
 
-    // Constructor for the object Employee
+    // Constructor for the object Employee with a given data
     public Employee(String nombre, int edad, int salario){
         this.name = nombre;
         this.age= edad;
         this.salary =salario;
         this.subordinates= new ArrayList<>(0);
 
+    }
+    // Constructor for Employee empty
+    public Employee(){
+        this.name = "";
+        this.age = 0;
+        this.salary = 0;
+    }
+
+    // Generate random data for an employee
+    public void randomData(){
+        //Random values for employees
+        int edadMin = 20;
+        int edadMax = 80;
+        int salarioMin = 80;
+        int salarioMax = 2000;
+        String[] nombres= {"Luis", "Maria", "Jesus", "Andrea","Angel","Laura","Miguel"};
+
+        // Randomly Generator
+        Random edadRandom = new Random();
+        Random salarioRandom  = new Random();
+        Random nombreRandom = new Random();
+
+        this.name= nombres[nombreRandom.nextInt(0, nombres.length)];
+        this.age = edadRandom.nextInt(edadMin,edadMax+1);
+        this.salary = salarioRandom.nextInt(salarioMin,salarioMax+1);
+        this.subordinates =new ArrayList<>(0);
     }
 
     /**
@@ -56,14 +83,16 @@ public class Employee {
      * @param person employe to calculate the count of successors
      * @return count of total subordinates with the condition
      */
-    public int employeesOldLower(Employee person){
+    public int employeesOldLowerRecursive(Employee person){
         // Variable to keep the subordinates with the condition
         int count=0;
         // Deeply Search for subordinates
+
         if (this.getSubordinates().size()>0) {
             for (Employee subordinado : this.getSubordinates()) {
                 if (subordinado.getAge() > person.getAge() && subordinado.getSalary() < person.getSalary()) {
                     count++;
+
                 }
                 // In case the subordinate doesn't follow the condition, but maybe for its subordinates is true
                 count += subordinado.employeesOldLower(person);
@@ -80,15 +109,15 @@ public class Employee {
      * @param person employe to calculate the count of successors
      * @return count of total subordinates with the condition
      */
-    public int employeesOldLowerRecursive(Employee person){
+    public int employeesOldLower(Employee person){
         // ArrayList Queue to keep all the subordinates of person
         MyArrayListQueue subordinates = new MyArrayListQueue();
         int count =0;
 
         // For the immediately subordinate verify the condition and change count
         for(Employee subordinate: this.getSubordinates()) {
+            subordinates.add(subordinate);
             if (subordinate.getAge() > person.getAge() && subordinate.getSalary() < person.getSalary()) {
-                subordinates.add(subordinate);
                 count ++;
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,32 @@
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
 
         // Create all employees in company
-        Employee empleado1 = new Employee("Fulano",46,2000);
-        Employee empleado2 = new Employee("Mengano",50,1000);
-        Employee empleado3 = new Employee("Sotano",51,1000);
-        Employee empleado4 = new Employee("Pengano",52,1000);
+        Employee empleado1 = new Employee();
+        empleado1.randomData();
+        Employee empleado2 = new Employee();
+        empleado2.randomData();
+        Employee empleado3 = new Employee();
+        empleado3.randomData();
+        Employee empleado4 = new Employee();
+        empleado4.randomData();
 
         // Assign only DIRECT subordinates for an employee
         empleado1.setSubordinates(empleado2);
         empleado2.setSubordinates(empleado3);
         empleado3.setSubordinates(empleado4);
 
-        // Test of two implementation
-        System.out.println("\n Calculando los subornidados aplicando recursiòn");
+
+        // Test of two implementation, showing employees
+        System.out.println("Nombre: " +empleado1.getName() + ". Edad: " + empleado1.getAge() + ". Salario: " + empleado1.getSalary());
+        System.out.println("Nombre: " +empleado2.getName() + ". Edad: " + empleado2.getAge() + ". Salario: " + empleado2.getSalary());
+        System.out.println("Nombre: " +empleado3.getName() + ". Edad: " + empleado3.getAge() + ". Salario: " + empleado3.getSalary());
+        System.out.println("Nombre: " +empleado4.getName() + ". Edad: " + empleado4.getAge() + ". Salario: " + empleado4.getSalary());
+
+        System.out.println("\n Calculando los subornidados con la condición (aplicando recursiòn)");
         System.out.println(empleado1.employeesOldLowerRecursive(empleado1));
-        System.out.println("\n Calculando los subordinados aplicando iteración y queues ");
+        System.out.println("\n Calculando los subordinados con la condición (aplicando iteración y queues)");
         System.out.println(empleado1.employeesOldLower(empleado1));
 
     }
